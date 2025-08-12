19293d1292bba8c6fd541ab910bdef3ac6d9bab8
edpau <edpau@me.com>
2025-08-12 14:10:31 +0100

    add back question and link onto code challenge Negative

--- src/main/scala/Negative.scala
+++ src/main/scala/Negative.scala
@@ -1,3 +1,18 @@
+/*
+https://www.codewars.com/kata/55685cd7ad70877c23000102/scala
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+
+Negative.makeNegative(1)  // return -1
+Negative.makeNegative(-5) // return -5
+Negative.makeNegative(0)  // return 0
+Notes
+
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+ */
+
 object Negative extends App {
 
   //  def makeNegative(n: Int): Int = {
